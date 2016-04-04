74f00193d1199ae7709c1782d583e94fda9181ba
Omri Spector <omri.spector@gmail.com>
2016-04-05 00:43:49 +0300

    Use database instead of a hash map

--- src/main/java/com/java2016/sbdemo/services/ArtistService.java
+++ src/main/java/com/java2016/sbdemo/services/ArtistService.java
@@ -1,25 +1,24 @@
 package com.java2016.sbdemo.services;
 
+import com.java2016.sbdemo.dao.ArtistRepository;
 import com.java2016.sbdemo.domain.Artist;
 import com.java2016.sbdemo.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class ArtistService {
-
-  private Map<Long,Artist> artistRepository = new HashMap<>();
+  @Autowired
+  private ArtistRepository artistRepository;
 
   public Artist addArtist(Artist artist) {
     artist.setId(System.currentTimeMillis());
-    artistRepository.put(artist.getId(),artist);
+    artistRepository.save(artist);
     return artist;
   }
 
   public Artist getArtistById(Long id) {
-    Artist ret =  artistRepository.get(id);
+    Artist  ret = artistRepository.findOne(id);
     if (ret==null) throw new ObjectNotFoundException("artist",id);
     return ret;
   }
