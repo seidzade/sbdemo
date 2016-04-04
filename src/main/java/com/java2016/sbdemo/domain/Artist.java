746ff14246e56a3bb38b73d2ae517d76d507b067
Omri Spector <omri.spector@gmail.com>
2016-04-05 00:42:02 +0300

    Artist is now an @Entity with an @Id

--- src/main/java/com/java2016/sbdemo/domain/Artist.java
+++ src/main/java/com/java2016/sbdemo/domain/Artist.java
@@ -2,8 +2,13 @@ package com.java2016.sbdemo.domain;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
+@Entity(name="artists")
 public class Artist {
+  @Id
   private long id;
   private String name;
   private int yearOfBirth;
