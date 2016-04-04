4d1e3df5be457e4008371ab10ac188ac2d2fbf08
Omri Spector <omri.spector@gmail.com>
2016-04-05 00:09:27 +0300

    service object - a managed bean with sterotype Service

---
+++ src/main/java/com/java2016/sbdemo/services/ArtistService.java
@@ -0,0 +1,19 @@
+package com.java2016.sbdemo.services;
+
+import com.java2016.sbdemo.domain.Artist;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ArtistService {
+
+  private Map<Long,Artist> artistRepository = new HashMap<>();
+
+  public Artist addArtist(Artist artist) {
+    artist.setId(System.currentTimeMillis());
+    artistRepository.put(artist.getId(),artist);
+    return artist;
+  }
+}
